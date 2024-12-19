935af0bd7e8656bcd485029b7d7a8522eeb3f1ca
Timofei <email@yandex.ru>
2024-12-19 19:28:03 +0300

    add

--- 19.12_PyGame5/main.py
+++ 19.12_PyGame5/main.py
@@ -1,14 +1,31 @@
 import os
 import sys
 import pygame
+from random import randrange
+
+
+class Bomb(pygame.sprite.Sprite):
+    def __init__(self, group, width, height):
+        super().__init__(group)
+        self.image = load_image("bomb.png")
+        self.rect = self.image.get_rect()
+        self.rect.x = randrange(width)
+        self.rect.y = randrange(height)
+        self.image_boom = load_image("boom.png")
+
+    def update(self, *args, **kwargs):
+        self.rect = self.rect.move(randrange(3) - 1, randrange(3) - 1)
+
+        if args and args[0].type == pygame.MOUSEBUTTONDOWN:
+            if self.rect.collidepoint(args[0].pos):
+                self.image = self.image_boom
 
 
 def main():
     # pygame setup
     pygame.init()
-    size = w, h = (1280, 720)
+    size = width, height = (1280, 720)
     screen = pygame.display.set_mode(size)
-    screen.fill("purple")
     clock = pygame.time.Clock()
     # image = load_image('owls.png', -1)
     #
@@ -16,13 +33,17 @@ def main():
     # image1 = pygame.transform.scale(robot, (200, 100))
     # screen.blit(image1, (700, 200))
     all_sprites = pygame.sprite.Group()
-    sprite = pygame.sprite.Sprite()
-    sprite.image = load_image("bomb.png", -1)
-    sprite.rect = sprite.image.get_rect()
-    sprite.rect.x = 100
-    sprite.rect.y = 200
-    all_sprites.add(sprite)
-
+    # sprite = pygame.sprite.Sprite()
+    # image = load_image("bomb.png", -1)
+    # for _ in range(100):
+    #     sprite = pygame.sprite.Sprite(all_sprites)
+    #     sprite.image = image
+    #     sprite.rect = sprite.image.get_rect()
+    #     sprite.rect.x = randrange(width)
+    #     sprite.rect.y = randrange(height)
+    #     all_sprites.add(sprite)
+    for _ in range(100):
+        Bomb(all_sprites, width, height)
     running = True
     while running:
         for event in pygame.event.get():
@@ -30,11 +51,14 @@ def main():
                 running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
                 all_sprites.draw(screen)
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                all_sprites.update(event)
             # if event.type == pygame.MOUSEBUTTONDOWN:
             #     screen.blit(image, event.pos)
-
+        screen.fill("purple")
+        all_sprites.draw(screen)
+        all_sprites.update()
         pygame.display.flip()
-
         clock.tick(60)
 
     pygame.quit()
